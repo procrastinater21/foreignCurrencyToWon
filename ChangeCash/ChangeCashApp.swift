e153880d7b3bdbfb7a1a8b940ebedce87199b025
fia <fia@tumblbug.com>
2023-07-04 13:54:21 +0900

    Initial Commit

---
+++ ChangeCash/ChangeCashApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChangeCashApp.swift
+//  ChangeCash
+//
+//  Created by fia on 2023/07/04.
+//
+
+import SwiftUI
+
+@main
+struct ChangeCashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
